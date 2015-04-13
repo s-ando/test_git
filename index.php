0242a6d658c946fa11319142341d335fa73348fc
andos <andos@istyle.co.jp>
2015-04-13 15:18:27 +0900

    test_git_4

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 <?php
 class hoge
-{
-    public function hogehoge(){
+{ 
+    public function hogehoge(){ 
 
-        $test = 'aaa';
-
-        if(is_numeric($test)){
-            echo $test;
-        }else{
-            echo 'do not numeric';
-        }
-    }
-}
+        $test = 'test_git_4'; 
+ 
+        if(is_numeric($test)){ 
+            echo $test; 
+        }else{ 
+            echo 'do not numeric'; 
+        } 
+    } 
+} 
 
 $obj = new hoge;
-echo $obj->hogehoge();
+echo $obj->hogehoge(); 
